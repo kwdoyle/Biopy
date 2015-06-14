6d0bc8e01258dcf5ca51bf54f67fffb6c290270e
kwdoyle <cheeze13579@gmail.com>
2015-06-14 14:43:41 -0400

    initial commit

---
+++ biopytest.py
@@ -0,0 +1,15 @@
+from Bio import SeqIO
+for seq_record in SeqIO.parse("outgroup sequence.fasta", "fasta"):
+    print(seq_record.id)
+    print(repr(seq_record.seq))
+    print(len(seq_record))
+
+my_seq = seq_record.seq
+
+print(repr(my_seq.complement()))
+print(repr(my_seq.reverse_complement()))
+
+# uncomment to print the entire sequence:
+# print(my_seq)
+# print(my_seq.complement())
+# print(my_seq.reverse_complement())
