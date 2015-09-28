b166df5f71216336cfb8253ef82458ad339654df
kwdoyle <cheeze13579@gmail.com>
2015-09-28 12:48:31 -0400

    script with just align function

---
+++ align.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from Bio import pairwise2
+from Bio.SubsMat import MatrixInfo as matlist
+
+matrix = matlist.blosum62
+
+# gap penalties:
+gap_open = -10
+gap_extend = -0.5
+
+def align:
+    algn = pairwise2.align.globalds(X, Y, matrix, gap_open, gap_extend)
+    best_align = algn[0]
+    return best_align
