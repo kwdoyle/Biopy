0db397cc03f21d3000167859f48bb7d38b250977
kwdoyle <cheeze13579@gmail.com>
2015-10-23 13:14:36 -0400

    show all possible best alignments

--- align_ex.py
+++ align_ex.py
@@ -19,10 +19,12 @@ X = "AACGTTGAC"
 
 # the 'ds' in align.globalXX means: A (d)ictionary returns the score of any pair of characters and (s)ame open and extend gap penalties for both sequences.
 # help regarding pairwise2 was found at http://biopython.org/DIST/docs/api/Bio.pairwise2-module.html
-algn = pairwise2.align.globalds(X, Y, matrix, gap_open, gap_extend)
+algn = pairwise2.align.globalds(a, b, matrix, gap_open, gap_extend)
+# pairwise2 will show all possible optimal alignments
 best_align = algn[0]
 print("Best alignment:")
 print(best_align)
+print algn
 # make nicer formatted alignment
 for i in algn:
     print(pairwise2.format_alignment(*i))  # the * takes all the values in the list and passes them as arguments for the function.
